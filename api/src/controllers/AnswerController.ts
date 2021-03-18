bbb406d4774c9d91fe5044b781f7aba3eba977ec
João Monteiro <joao.monteiro@hiperferro.com.br>
2021-03-18 17:24:54 -0300

    answerController

---
+++ api/src/controllers/AnswerController.ts
@@ -0,0 +1,31 @@
+import { request, response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+
+class AnswerController {
+
+    async execute(request: Request, response: Response) {
+        const { value } = request.params;
+        const { u } = request.query;
+
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u)
+        });
+
+        if(!surveyUser) {
+            return response.status(400).json({
+                error: "Surveys user does not exists!"
+            });
+        }
+
+        surveyUser.value = Number(value);
+        
+        await surveysUsersRepository.save(surveyUser);
+
+        return response.json(surveyUser);
+    }
+}
+
+export { AnswerController }
